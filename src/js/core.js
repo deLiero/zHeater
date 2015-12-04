dbeb0954fa9b6fb891b5ecb62ec5a8d8fc0f740a
Alexander Khramushin <alkh34@yandex.ru>
2015-12-04 17:49:58 +0300

    макет ядра

--- src/js/core.js
+++ src/js/core.js
@@ -1,4 +1,30 @@
 // core
 function Core(require, exports) {
     var Sandbox = require("Sandbox");
+
+    var bind = function (func, context) {
+        return function () {
+            return func.apply(context, arguments);
+        }
+    };
+
+    // namespace
+
+    var Core = {
+        init: function () {
+
+        },
+
+        _initModules: function () {
+
+        },
+
+        initModule: function () {
+
+        },
+
+        destroyModule: function () {
+
+        }
+    };
 }
