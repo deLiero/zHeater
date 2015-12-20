95bd941a9116ce38166223c02e50e937bfa1d263
de_Liero <alkh34@yandex.ru>
2015-12-21 00:25:39 +0300

    добавлен показ значений при наведии курсора мыши

--- src/js/descriptors.js
+++ src/js/descriptors.js
@@ -1,6 +1,6 @@
 zHeater["descriptors"] = {
     "Form": {name: "Form", resources: {inputs: {diameter: "D", height: "H", gap: "gap", thickness: "Tk"}}},
     "Calculator": {name: "Calculator"},
-    "Printer": {"name": "Printer", "resources": {"table": "table"}},
+    "Printer": {"name": "Printer", "resources": {"table": "table", "printer_left": "printer_left"}},
     "Helper": {"name": "Helper"}
 }
